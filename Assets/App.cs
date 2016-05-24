bd7d92c04325212a83b9dff93ec45c99bbcfae6c
Shin Ishimaru <shinishimaru@gmail.com>
2016-05-24 15:19:26 +0900

    first commit

---
+++ Assets/App.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class App : MonoBehaviour {
+
+	GameObject m_ColorChangeObj = null;
+	public Material m_SphereMaterial;
+	public Material m_SelectMaterial;
+	public Material m_CubeMaterial;
+	public GameObject m_CubeObj;
+
+	bool m_LayerMask = true;
+
+	// Use this for initialization
+	void Start () {
+
+		int num = 10;
+		for (int x = -num; x < num; ++x) {
+			for (int y = -num; y < num; ++y) {
+				Vector3 pos = new Vector3 (0.5f + (float)x, 0.5f + (float)y, 4.0f);
+				Instantiate (m_CubeObj,
+					pos,
+					Quaternion.identity);
+			}
+		}
+
+	}
+
+	void ReturnMaterial()
+	{
+		if (m_ColorChangeObj) {
+			if (m_ColorChangeObj.name == "Sphere") {
+				m_ColorChangeObj.GetComponent<Renderer> ().material = m_SphereMaterial;
+			} else {
+				m_ColorChangeObj.GetComponent<Renderer> ().material = m_CubeMaterial;
+			}
+		}
+		m_ColorChangeObj = null;
+
+	}
+
+	void OnGUI () {
+		// テキストフィールドを表示する
+		GUI.TextArea(new Rect(10, 10, 200, 100), "Push Z To ON LayerMask.\nPush X To OFF LayerMask.");
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Z)) {
+			m_LayerMask = true;
+			Debug.Log ("LayerMask ON");
+		}
+		if (Input.GetKeyDown (KeyCode.X)) {
+			m_LayerMask = false;
+			Debug.Log ("LayerMask OFF");
+		}
+
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		// http://docs.unity3d.com/ja/current/Manual/Layers.html
+		// ここを参考に
+
+		// Bit shift the index of the layer (8) and layer (9) to get a bit mask
+		//int layerMask = 1 << 9 | 1 << 8;
+
+		// Bit shift the index of the layer (9) to get a bit mask
+		int layerMask = 1 << 9;
+
+		// This would cast rays only against colliders in layer 9.
+		// But instead we want to collide against everything except layer 9. The ~ operator does this, it inverts a bitmask.
+		layerMask = ~layerMask;
+
+		if (!m_LayerMask) {
+			layerMask = 0xfffffff;
+		}
+
+		if (!Physics.Raycast (ray, out hit, Mathf.Infinity, layerMask)) {
+			ReturnMaterial ();
+			return;
+		}
+
+		if (m_ColorChangeObj != hit.collider.gameObject) {
+			ReturnMaterial ();
+		}
+
+		m_ColorChangeObj = hit.collider.gameObject;
+		hit.collider.gameObject.GetComponent<Renderer> ().material = m_SelectMaterial;
+
+
+	}
+}
